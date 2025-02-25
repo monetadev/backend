b29e899dcd2d5d5615d4b26dedea9834f2b86478
MatthewDeSouza <me@bipp.dev>
2025-02-24 22:27:08 -0500

    Update test package.

--- src/test/java/com/github/monetadev/backend/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.github.monetadev.backend;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
