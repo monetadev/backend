a8d437cc960f632a8b7868b81b4a753193359f53
MatthewDeSouza <me@bipp.dev>
2025-03-19 21:04:42 -0400

    Update repository layer.

--- src/main/java/com/github/monetadev/backend/repository/FlashcardRepository.java
+++ src/main/java/com/github/monetadev/backend/repository/FlashcardRepository.java
@@ -1,13 +1,14 @@
 package com.github.monetadev.backend.repository;
 
 import com.github.monetadev.backend.model.Flashcard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FlashcardRepository extends JpaRepository<Flashcard, UUID> {
-    List<Flashcard> findAllByFlashcardSetId(UUID flashcardSetId);
+    Page<Flashcard> findAllByFlashcardSetId(UUID flashcardSetId, Pageable pageable);
 }
