35153efd5449ea0ccb1a9e403964db9901e30378
MatthewDeSouza <me@bipp.dev>
2025-03-19 20:57:37 -0400

    Update JWT configuration.

--- src/main/java/com/github/monetadev/backend/config/JwtConfig.java
+++ src/main/java/com/github/monetadev/backend/config/JwtConfig.java
@@ -11,27 +11,51 @@ import java.time.Duration;
 
 @Configuration
 public class JwtConfig {
+    @Value("${jwt.cookie.name:jwt}")
+    private String cookieName;
+
+    @Value("${jwt.cookie.maxAgeInSeconds:86400}")
+    private int cookieMaxAge;
+
+    @Value("${jwt.cookie.path:/}")
+    private String cookiePath;
+
+    @Value("${jwt.cookie.httpsOnly:false}")
+    private Boolean secured;
+
+    @Value("${jwt.cookie.same-site:Lax}")
+    private String sameSitePolicy;
+
     @Value("${jwt.secret}")
     private String secret;
 
-    @Value("${jwt.expiration:86400}")
-    private Long expirationSeconds;
-
-    @Value("${jwt.secured:false}")
-    private Boolean secured;
-
     @Bean
-    public SecretKey secretKey() {
-        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    public String cookieName() {
+        return cookieName;
     }
 
     @Bean
-    public Duration expiration() {
-        return Duration.ofSeconds(expirationSeconds);
+    public Duration cookieMaxAge() {
+        return Duration.ofSeconds(cookieMaxAge);
+    }
+
+    @Bean
+    public String cookiePath() {
+        return cookiePath;
     }
 
     @Bean
     public Boolean isSecured() {
         return secured;
+    }
+
+    @Bean
+    public String sameSitePolicy() {
+        return sameSitePolicy;
+    }
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 }
---
+++ src/main/java/com/github/monetadev/backend/security/jwt/JwtCookieInterceptor.java
@@ -0,0 +1,72 @@
+package com.github.monetadev.backend.security.jwt;
+
+import graphql.GraphQLContext;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.server.WebGraphQlInterceptor;
+import org.springframework.graphql.server.WebGraphQlRequest;
+import org.springframework.graphql.server.WebGraphQlResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+
+@Component
+public class JwtCookieInterceptor implements WebGraphQlInterceptor {
+    private final JwtService jwtService;
+    private final String cookieName;
+    private final Duration cookieMaxAge;
+    private final String cookiePath;
+    private final Boolean isSecure;
+    private final String sameSitePolicy;
+
+    public JwtCookieInterceptor(JwtService jwtService,
+                                @Autowired String cookieName,
+                                @Autowired Duration cookieMaxAge,
+                                @Autowired String cookiePath,
+                                @Autowired Boolean isSecure,
+                                @Autowired String sameSitePolicy) {
+        this.jwtService = jwtService;
+        this.cookieName = cookieName;
+        this.cookieMaxAge = cookieMaxAge;
+        this.cookiePath = cookiePath;
+        this.isSecure = isSecure;
+        this.sameSitePolicy = sameSitePolicy;
+    }
+
+    @NotNull
+    @Override
+    public Mono<WebGraphQlResponse> intercept(@NotNull WebGraphQlRequest request, Chain chain) {
+        return chain.next(request).doOnNext(response -> {
+            GraphQLContext context = response.getExecutionInput().getGraphQLContext();
+            if (context == null) return;
+
+            Object tokenObject = context.get(cookieName);
+            if (!(tokenObject instanceof String token)) return;
+            if (!jwtService.isValidToken(token)) return;
+
+            try {
+                String cookieValue = buildCookie(token);
+                response.getResponseHeaders().add(HttpHeaders.SET_COOKIE, cookieValue);
+            } catch (Exception ignored) {
+                // We shouldn't disrupt the response flow.
+                // TODO: Implement logging in rest of class.
+            }
+        });
+    }
+
+    private String buildCookie(String token) {
+        StringBuilder cookieBuilder = new StringBuilder();
+        cookieBuilder.append(cookieName).append("=").append(token).append("; ");
+        cookieBuilder.append("Path=").append(cookiePath).append("; ");
+        cookieBuilder.append("Max-Age=").append(cookieMaxAge.toSeconds()).append("; ");
+        cookieBuilder.append("HttpOnly; ");
+        if (isSecure){
+            cookieBuilder.append("Secure; ");
+        }
+        cookieBuilder.append("SameSite=").append(sameSitePolicy).append(";");
+        return cookieBuilder.toString();
+    }
+}
